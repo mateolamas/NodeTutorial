a80333088ae650d5f4dbc9aa46f22e1247594768
mateo <mateo.lamas@udc.es>
2024-02-13 10:49:30 +0100

    Servidor http y conexion a la BBDD

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-import { printHello } from '#Lib/hello.js';
+import httpServer from '#Config/http.js'
+import connectDB from '#Config/db.js';
+import '#Config/env.js';
 
-printHello();
+
+const bootstrap = async () => {
+
+    await connectDB(process.env.MONGODB_URL);
+
+    httpServer.listen(process.env.PORT, () => {
+        console.log(`Escuchando en el puerto ${process.env.PORT}`);
+    })
+};
+
+bootstrap();
+
+
+
+
+
+
+
+
